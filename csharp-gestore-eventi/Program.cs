e965293991c46b8ce7accc468ebe61b4847bda70
luigitarallo <tarallo.gigi@gmail.com>
2024-05-02 13:01:00 +0200

    end milestone 3

--- csharp-gestore-eventi/Program.cs
+++ csharp-gestore-eventi/Program.cs
@@ -4,7 +4,56 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+
+                Console.Write("Inserisci il nome dell'evento: ");
+                string titolo = Console.ReadLine();
+                Console.Write("Inserisci la data dell' evento (formato data gg/MM/yyyy): ");
+                DateTime data = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+                Console.Write("Inserisci il numero dei posti totali: ");
+                int capienzaMax = int.Parse(Console.ReadLine());
+
+                Evento nuovoEvento = new Evento(titolo, data, capienzaMax);
+
+                Console.Write("Vuoi prenotare dei posti per questo evento? (digita 's' per sì / 'n' per no): ");
+                string risposta = Console.ReadLine();
+                while(risposta.ToLower() == "s")
+                {
+                    Console.Write("Quanti posti vuoi prenotare? ");
+                    int postiDaPrenotare = int.Parse(Console.ReadLine());
+                    nuovoEvento.PrenotaPosti(postiDaPrenotare);
+
+                    Console.WriteLine($"Posti prenotati: {nuovoEvento.PostiPrenotati}");
+                    Console.WriteLine($"Posti disponibili: {nuovoEvento.CapienzaMax - nuovoEvento.PostiPrenotati}");
+                    Console.Write("Vuoi prenotare altri posti? (digita 's' per sì / 'n' per no): ");
+                    risposta = Console.ReadLine();
+                }
+
+                Console.Write("Vuoi disdire dei posti per questo evento? (digita 's' per sì / 'n' per no): ");
+                risposta = Console.ReadLine();
+                while(risposta.ToLower() == "s")
+                {
+                    Console.Write("Quanti posti vuoi disdire? ");
+                    int postiDaDisdire = int.Parse(Console.ReadLine());
+                    nuovoEvento.DisdiciPosti(postiDaDisdire);
+                    Console.WriteLine($"Posti prenotati: {nuovoEvento.PostiPrenotati}");
+                    Console.WriteLine($"Posti disponibili: {nuovoEvento.CapienzaMax - nuovoEvento.PostiPrenotati}");
+                    Console.Write("Vuoi disdire altri posti? (digita 's' per sì / 'n' per no): ");
+                    risposta = Console.ReadLine();
+                }
+                if(risposta.ToLower() == "n")
+                {
+                    Console.WriteLine("Ok va bene!");
+                    Console.WriteLine($"Posti prenotati: {nuovoEvento.PostiPrenotati}");
+                    Console.WriteLine($"Posti disponibili: {nuovoEvento.CapienzaMax - nuovoEvento.PostiPrenotati}");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Errore: {ex.Message}");
+            }
         }
     }
 }
---
+++ csharp-gestore-eventi/ProgrammaEventi.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    public class ProgrammaEventi
+    {
+        private string _titolo;
+        private List<Evento> eventi;
+
+        public string Titolo
+        { get { return _titolo; }
+        private set { _titolo = value; }
+        }
+
+        public ProgrammaEventi(string titolo)
+        {
+            Titolo = titolo;
+            eventi = new List<Evento>();
+        }
+
+        public void AggiungiEvento(Evento nuovoEvento)
+        {
+            eventi.Add(nuovoEvento);
+        }
+        public List<Evento> EventiInData(DateTime data)
+        {
+            List<Evento> eventiInData = new List<Evento>();
+
+            foreach(Evento evento in eventi)
+            {
+                if(evento.Data.Date == data.Date)
+                {
+                    eventiInData.Add(evento);
+                }
+            }
+            return eventiInData;
+        }
+
+        public static string StampaEventi(List<Evento> listaEventi)
+        {
+            string output = "";
+            foreach (Evento evento in listaEventi)
+            {
+                output += evento.ToString() + "\n";
+            }
+            return output;
+        }
+
+        public int NumeroEventi()
+        {
+            return eventi.Count;
+        }
+
+        public void SvuotaEventi()
+        {
+            eventi.Clear();
+        }
+
+        public override string ToString()
+        {
+            string output = $"Nome programma evento: {Titolo}\n";
+            foreach(Evento evento in eventi)
+            {
+                output += evento.ToString() + "\n";
+            }
+            return output;
+        }
+
+    }
+}
